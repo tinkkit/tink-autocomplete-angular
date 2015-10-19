de0f7b477c2f3f5c026346ded213afddfd8668ac
Jasper Van Proeyen <trianglejuice@mac.com>
2015-10-19 17:05:06 +0200

    Added basic styling (WIP)

--- dist/scripts/templates.js
+++
@@ -1,8 +0,0 @@
-angular.module('tink.autocomplete').run(['$templateCache', function($templateCache) {
-  'use strict';
-
-  $templateCache.put('templates/autocomplete.html',
-    "<div> <input ng-change=ctrl.inputChange() ng-model=ctrl.inputValue id=input-text placeholder=\"input[type=&quot;text&quot;]\"> <div class=tinkAutoComplete> <div class=tinkNgRepeatTransclude ng-mousedown=ctrl.setModelData($event,tinkArray)> </div> </div> </div>"
-  );
-
-}]);
